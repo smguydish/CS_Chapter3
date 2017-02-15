650ccd2d11fc09fbf002b443bdaacd67fcc10fe9
smguydish <smguydish@gmail.com>
2017-02-15 13:42:39 -0500

    check low rate project

--- CS_Chapter3/CheckLowRate/Program.cs
+++ CS_Chapter3/CheckLowRate/Program.cs
@@ -9,6 +9,17 @@ namespace CheckLowRate
     {
         static void Main(string[] args)
         {
+            int pay = Convert.ToInt32(Console.ReadLine());
+
+            if (pay < 5.56)
+            {
+                Console.WriteLine("ERROR!");
+            }
+            
+            else
+            {
+                Console.WriteLine("well done.");
+            }        
         }
     }
 }
