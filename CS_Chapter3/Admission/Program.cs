0641bec774d0bb0ab4b3f2034c8db01bc1480b47
smguydish <smguydish@gmail.com>
2017-02-24 13:45:45 -0500

    Admission Project

--- CS_Chapter3/Admission/Program.cs
+++ CS_Chapter3/Admission/Program.cs
@@ -9,7 +9,20 @@ namespace Admission
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter GPA: ");
+            double gpa = Convert.ToDouble(Console.ReadLine());
 
+            Console.Write("Enter admission test score: ");
+            int score = Convert.ToInt32(Console.ReadLine());
+
+            if ((gpa >= 3.0 && score >= 60) || (gpa < 3.0 && score >= 80))
+            {
+                Console.WriteLine("Accept");
+            }
+            else
+            {
+                Console.WriteLine("Decline");
+            }
         }
     }
 }
